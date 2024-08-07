037404dc5f7fde21402ef7ccfdd0201ce24eb83e
2405-011-pw <walypa56@gmail.com>
2024-08-07 16:22:28 +0100

    user map done no button functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,31 @@
-import { DemonstratingProps } from "./components/demo/DemonstratingProps";
-
-//@ts-ignore ignore typescript issues with import.meta
-const apiBaseURL = import.meta.env.VITE_API_BASE_URL;
+import axios from "axios";
+import React from "react";
 
 function App() {
+    const [movieData, setMovieData] = React.useState([]);
+    React.useEffect(() => {
+        axios.get("http://localhost:4000/api/coffeeData/").then(({ data }) => {
+            setMovieData(data);
+        });
+    }, []);
+
+    const memberList = movieData.map((memberObj) => {
+        return (
+            <div className="member" key={memberObj.id}>
+                <p>
+                    {memberObj.name} - {memberObj.stamps} stamp(s),{" "}
+                    {memberObj.freeCoffees} free coffee(s) available
+                </p>
+                <button>add stamp</button>
+                <button>redeem free coffee</button>
+            </div>
+        );
+    });
+
     return (
         <div>
-            <h1>App component</h1>
-            <DemonstratingProps />
-            <p>
-                The value of the environment variable VITE_API_BASE_URL is:
-                <code>{apiBaseURL}</code>
-            </p>
+            <h1>Coffee Members</h1>
+            {memberList}
         </div>
     );
 }
