c65810d19d99125da9c1635fa37c831cbbc59814
2405-011-pw <walypa56@gmail.com>
2024-08-07 16:32:03 +0100

    add stamp and redeem free coffee working

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
         axios.get("http://localhost:4000/api/coffeeData/").then(({ data }) => {
             setMovieData(data);
         });
-    }, []);
+    }, [movieData]);
 
     const memberList = movieData.map((memberObj) => {
         return (
@@ -16,11 +16,19 @@ function App() {
                     {memberObj.name} - {memberObj.stamps} stamp(s),{" "}
                     {memberObj.freeCoffees} free coffee(s) available
                 </p>
-                <button>add stamp</button>
-                <button>redeem free coffee</button>
+                <button onClick={() => addStamp(memberObj.id)}>add stamp</button>
+                <button onClick={() => redeemFreeCoffee(memberObj.id)}>redeem free coffee</button>
             </div>
         );
     });
+
+    function addStamp(id) {
+        axios.put(`http://localhost:4000/api/coffeeData/addStamp/${id}`)
+    }
+
+    function redeemFreeCoffee(id) {
+        axios.put(`http://localhost:4000/api/coffeeData/redeemFreeCoffee/${id}`)
+    }
 
     return (
         <div>
